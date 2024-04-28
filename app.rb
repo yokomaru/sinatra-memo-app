1fe59a4d251e104aa8991812d63a2c0d0625ff3c
yokomaru <cabochetoganlan@gmail.com>
2024-04-28 23:03:44 +0900

    feat: add all memos uri to app.rb
    作成するメモアプリに使用するURIとHTTPメソッドを追加

--- app.rb
+++ app.rb
@@ -2,5 +2,33 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-  'Hello world!'
+  'トップページ'
+end
+
+get '/memos' do
+  'メモ一覧ページ'
+end
+
+get '/memos/new' do
+  '新しいメモの作成ページ'
+end
+
+get '/memos/:id' do |id|
+  "メモの編集ページ id = #{id}"
+end
+
+get '/memos/:id/edit' do |id|
+  "指定したidのメモの内容を取得する id = #{id}"
+end
+
+post '/memos' do
+  '新しいメモを追加する'
+end
+
+patch '/memos/:id' do |id|
+  "指定したidのメモを更新する id = #{id}"
+end
+
+delete '/memos/:id' do |id|
+  "指定したidのメモを削除する id = #{id}"
 end
