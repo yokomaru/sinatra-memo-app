fba81d032502031f4ab111e2407a2057d4c200b5
yokomaru <cabochetoganlan@gmail.com>
2024-06-02 23:08:52 +0900

    update: separate memos model from app.rb
    Memoクラスをapp.rbから分ける
    ファイルの更新処理をMemoクラスにまとめる
    XSS対応処理の追加

--- app.rb
+++ app.rb
@@ -1,13 +1,17 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require './models/memos'
-require 'json'
 
 enable :method_override
 
+helpers do
+  def h(text)
+    Rack::Utils.escape_html(text)
+  end
+end
+
 get '/' do
-  #@memos = Memo.all
-  File.open('public/memos.json') { |file| JSON.load(file) } || []
+  @memos = Memo.all
   erb :index
 end
 
@@ -20,67 +24,44 @@ get '/memos/new' do
 end
 
 post '/memos' do
-  # Memo.all
-  @memos = File.open('public/memos.json') { |file| JSON.load(file) } || []
-  max_id = @memos.empty? ? 1 : @memos.map{|memo| memo["id"].to_i}.max + 1
-  hash = {"id": max_id.to_s, "title": params[:title], "content": params[:content]}
+  @memos = Memo.all
+  max_id = Memo.fetch_max_id(@memos)
+  hash = {"id": max_id.to_s, "title": h(params[:title]), "content": h(params[:content])}
+  # 破壊的なのでdupする
   @memos.push(hash.dup)
-  # Memo.save
-  File.open('public/memos.json', 'w') do |file|
-    str = JSON.dump(@memos, file)
-  end
+  Memo.save(@memos)
   redirect "/memos/#{max_id}"
 end
 
 get '/memos/:id' do |id|
-  # Memo.all
-  @memos = File.open('public/memos.json') { |file| JSON.load(file) }
-  # Memo.find_by_id(id)
-  @memo = @memos.find {|x| x["id"].include?(id)}
+  @memos = Memo.all
+  @memo = Memo.find_by_id(@memos, id)
 	erb :show
 end
 
 get '/memos/:id/edit' do |id|
-  # Memo.all
-  @memos = File.open('public/memos.json') { |file| JSON.load(file) }
-  # Memo.find_by_id(id)
-  @memo = @memos.find {|x| x["id"].include?(id)}
+  @memos = Memo.all
+  @memo = Memo.find_by_id(@memos, id)
 	erb :edit
 end
 
 patch '/memos/:id' do |id|
-  # Memo.all
-  @memos = File.open('public/memos.json') { |file| JSON.load(file) }
-  # Memo.find_by_id(id)
-  @memo = @memos.find {|x| x["id"].include?(id)}
+  @memos = Memo.all
+  @memo = Memo.find_by_id(@memos, id)
   @memos.find do |memo|
     if memo["id"].include?(id)
-      memo['title'] = params[:title]
-      memo['content'] = params[:content]
+      memo['title'] = h(params[:title])
+      memo['content'] = h(params[:content])
     end
   end
-  # Memo.save
-  File.open('public/memos.json', 'w') do |file|
-    str = JSON.dump(@memos, file)
-  end
+  Memo.save(@memos)
   redirect to "/memos/#{params[:id]}"
 end
 
 delete '/memos/:id' do |id|
-  #Memo.find_by_id(params[:id])
-  @memo = @memos.find {|x| x["id"].include?(id)}
-  # Memo.all
-  @memos = File.open('public/memos.json') { |file| JSON.load(file) }
-  # Memo.destroy
-  @memos.find.with_index do |memo, i|
-    if memo["id"].include?(id)
-      @memos.delete_at(i)
-    end
-  end
-  # Memo.save
-  File.open('public/memos.json', 'w') do |file|
-    str = JSON.dump(@memos, file)
-  end
+  @memos = Memo.all
+  @memo = Memo.find_by_id(@memos, id)
+  Memo.destroy(@memos, id)
+  Memo.save(@memos)
   redirect '/'
-  erb :index
 end
---
+++ models/memos.rb
@@ -0,0 +1,30 @@
+require 'json'
+
+class Memo
+  def self.all
+    File.open('public/memos.json') { |file| JSON.load(file) } || []
+  end
+
+  def self.find_by_id(memos, id)
+    memos.find {|x| x["id"].include?(id)}
+  end
+
+  def self.save(memos)
+    File.open('public/memos.json', 'w') do |file|
+      str = JSON.dump(memos, file)
+    end
+  end
+
+  def self.destroy(memos, id)
+    memos.find.with_index do |memo, i|
+      if memo["id"].include?(id)
+        memos.delete_at(i)
+      end
+    end
+  end
+
+  def self.fetch_max_id(memos)
+    memos.empty? ? 1 : memos.map{|memo| memo["id"].to_i}.max + 1
+  end
+
+end
