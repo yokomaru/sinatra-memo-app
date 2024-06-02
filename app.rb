014ddb210f3ade4b03c2a8fc3532fc631d47dae8
yokomaru <cabochetoganlan@gmail.com>
2024-06-02 19:11:11 +0900

    update: not use activerecord and use json file
    仮で使用したActiveRecordでの処理を削除し、jsonファイルへのCRUD処理を追加
    memos.jsonを追加
    erbファイルの表示をハッシュに置き換え

--- app.rb
+++ app.rb
@@ -1,19 +1,13 @@
 require 'sinatra'
 require 'sinatra/reloader'
-require 'active_record'
-
-ActiveRecord::Base.establish_connection(
-    "adapter" => "sqlite3",
-    "database" => "memo.db"
-)
-
-class Memo < ActiveRecord::Base
-end
+require './models/memos'
+require 'json'
 
 enable :method_override
 
 get '/' do
-  @memos = Memo.order("id desc").all
+  #@memos = Memo.all
+  File.open('public/memos.json') { |file| JSON.load(file) } || []
   erb :index
 end
 
@@ -26,30 +20,67 @@ get '/memos/new' do
 end
 
 post '/memos' do
-  @memo = Memo.create({title: params[:title], content: params[:content]})
-  redirect "/memos/#{@memo.id}"
+  # Memo.all
+  @memos = File.open('public/memos.json') { |file| JSON.load(file) } || []
+  max_id = @memos.empty? ? 1 : @memos.map{|memo| memo["id"].to_i}.max + 1
+  hash = {"id": max_id.to_s, "title": params[:title], "content": params[:content]}
+  @memos.push(hash.dup)
+  # Memo.save
+  File.open('public/memos.json', 'w') do |file|
+    str = JSON.dump(@memos, file)
+  end
+  redirect "/memos/#{max_id}"
 end
 
 get '/memos/:id' do |id|
-  @memo = Memo.find_by_id(params[:id])
+  # Memo.all
+  @memos = File.open('public/memos.json') { |file| JSON.load(file) }
+  # Memo.find_by_id(id)
+  @memo = @memos.find {|x| x["id"].include?(id)}
 	erb :show
 end
 
 get '/memos/:id/edit' do |id|
-  @memo = Memo.find_by_id(params[:id])
+  # Memo.all
+  @memos = File.open('public/memos.json') { |file| JSON.load(file) }
+  # Memo.find_by_id(id)
+  @memo = @memos.find {|x| x["id"].include?(id)}
 	erb :edit
 end
 
 patch '/memos/:id' do |id|
-  @memo = Memo.find_by_id(params[:id])
-	@memo.title = params[:title]
-  @memo.content = params[:content]
-	@memo.save
-	redirect to "/memos/#{params[:id]}"
+  # Memo.all
+  @memos = File.open('public/memos.json') { |file| JSON.load(file) }
+  # Memo.find_by_id(id)
+  @memo = @memos.find {|x| x["id"].include?(id)}
+  @memos.find do |memo|
+    if memo["id"].include?(id)
+      memo['title'] = params[:title]
+      memo['content'] = params[:content]
+    end
+  end
+  # Memo.save
+  File.open('public/memos.json', 'w') do |file|
+    str = JSON.dump(@memos, file)
+  end
+  redirect to "/memos/#{params[:id]}"
 end
 
 delete '/memos/:id' do |id|
-  Memo.find(params[:id]).destroy
+  #Memo.find_by_id(params[:id])
+  @memo = @memos.find {|x| x["id"].include?(id)}
+  # Memo.all
+  @memos = File.open('public/memos.json') { |file| JSON.load(file) }
+  # Memo.destroy
+  @memos.find.with_index do |memo, i|
+    if memo["id"].include?(id)
+      @memos.delete_at(i)
+    end
+  end
+  # Memo.save
+  File.open('public/memos.json', 'w') do |file|
+    str = JSON.dump(@memos, file)
+  end
   redirect '/'
   erb :index
 end
