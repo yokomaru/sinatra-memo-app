1098a45bba5a8d8d9753b1df96a338bf8fae85f3
yokomaru <cabochetoganlan@gmail.com>
2024-06-02 14:03:26 +0900

    add: crud and erb
    activerecordを使って各CRUDの処理を追加
    CRUDに該当するviewファイルを追加

--- app.rb
+++ app.rb
@@ -1,23 +1,19 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'active_record'
 
-before do
-  @author = "yokomaru"
+ActiveRecord::Base.establish_connection(
+    "adapter" => "sqlite3",
+    "database" => "memo.db"
+)
+
+class Memo < ActiveRecord::Base
 end
 
-after do
-  logger.info "page displayed"
-end
-
-helpers do
-  def  strong(a)
-   "<strong>#{a}</strong>"
-  end
-end
+enable :method_override
 
 get '/' do
-  @title = "index"
-  @content = "index #{strong(@author)}"
+  @memos = Memo.order("id desc").all
   erb :index
 end
 
@@ -26,25 +22,34 @@ get '/memos' do
 end
 
 get '/memos/new' do
-  '新しいメモの作成ページ'
-end
-
-get '/memos/:id' do |id|
-  "メモの編集ページ id = #{id}"
-end
-
-get '/memos/:id/edit' do |id|
-  "指定したidのメモの内容を取得する id = #{id}"
+  erb :new
 end
 
 post '/memos' do
-  '新しいメモを追加する'
+  @memo = Memo.create({title: params[:title], content: params[:content]})
+  redirect "/memos/#{@memo.id}"
+end
+
+get '/memos/:id' do |id|
+  @memo = Memo.find_by_id(params[:id])
+	erb :show
+end
+
+get '/memos/:id/edit' do |id|
+  @memo = Memo.find_by_id(params[:id])
+	erb :edit
 end
 
 patch '/memos/:id' do |id|
-  "指定したidのメモを更新する id = #{id}"
+  @memo = Memo.find_by_id(params[:id])
+	@memo.title = params[:title]
+  @memo.content = params[:content]
+	@memo.save
+	redirect to "/memos/#{params[:id]}"
 end
 
 delete '/memos/:id' do |id|
-  "指定したidのメモを削除する id = #{id}"
+  Memo.find(params[:id]).destroy
+  redirect '/'
+  erb :index
 end
