2fd9fc05d9eb322a89ca326336ad9c4941dfe05a
yokomaru <cabochetoganlan@gmail.com>
2024-06-15 18:31:23 +0900

    fix: change memo method
    ファイル更新処理を一旦仮のDB接続処理に置き換え

--- models/memo.rb
+++ models/memo.rb
@@ -1,56 +1,47 @@
 # frozen_string_literal: true
 
 require 'json'
+require 'pg'
 
 class Memo
   FILE_PATH = 'db/memo.json'
 
   def self.all
-    File.open(FILE_PATH, 'r') do |file|
-      content = file.read
-      content.empty? ? [] : JSON.parse(content)
-    end
+    conn = set_connect
+    results = conn.exec("SELECT * FROM meos")
+    conn.close
+    results
   end
 
   def self.find_by_id(id)
-    memos = Memo.all
-    memos.find { |x| x['id'] == id }
+    conn = set_connect
+    memo = conn.exec("SELECT * FROM memos where id = #{id}").to_a
+    memo[0]
   end
 
-  def self.save(memos)
-    File.open(FILE_PATH, 'w') do |file|
-      JSON.dump(memos, file)
-    end
-  end
 
   def self.create(params)
-    memos = Memo.all
-    max_id = Memo.fetch_max_id(memos)
-    memos << { 'id': max_id.to_s, 'title': params[:title], 'content': params[:content] }
-    Memo.save(memos)
+
+    conn = set_connect
+    conn.exec("INSERT INTO memos (title, content) VALUES ('#{params[:title]}', '#{params[:content]}')")
+    results = conn.exec("select * from memos")
+    conn.close
+    true
   end
 
   def self.update(params)
-    memos = Memo.all
-    memos.find do |memo|
-      if memo['id'] == params[:id]
-        memo['title'] = params[:title]
-        memo['content'] = params[:content]
-      end
-    end
-    Memo.save(memos)
-  end
 
+    conn = set_connect
+    conn.exec("UPDATE memos SET title = '#{params[:title]}', content = '#{params[:content]}'  where id = #{params[:id]}")
+    results = conn.exec("select * from memos")
+    conn.close
+    true
+  end
   def self.destroy(id)
-    memos = Memo.all
-    memos.find.with_index do |memo, i|
-      memos.delete_at(i) if memo['id'] == id
-    end
-    Memo.save(memos)
-  end
-
-  def self.fetch_max_id(memos)
-    memos.empty? ? 1 : memos.map { |memo| memo['id'].to_i }.max + 1
+    conn = set_connect
+    conn.exec("DELETE FROM meos where id = #{id}")
+    conn.close
+    true
   end
 
   def self.create_db
@@ -59,5 +50,10 @@ class Memo
     File.open(FILE_PATH, 'w') do |file|
       JSON.dump([], file)
     end
+  end
+
+  private
+  def self.set_connect
+    PG.connect(dbname: 'postgres')
   end
 end
