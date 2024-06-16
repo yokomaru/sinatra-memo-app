b50cb37cb420bae4a34118926b7dbb5f8d35bfec
yokomaru <cabochetoganlan@gmail.com>
2024-06-17 01:10:16 +0900

    update: setup.rb
    初期のDB・テーブル作成処理に存在チェックを追加

--- db/setup.rb
+++ db/setup.rb
@@ -1,7 +1,22 @@
 require 'pg'
 
-conn = PG.connect(dbname: 'postgres')
-conn.exec("DROP DATABASE sinatra_memo_app")
-conn.exec("CREATE DATABASE sinatra_memo_app")
-conn.exec("CREATE TABLE memos (id serial PRIMARY KEY, title varchar(100), content varchar(10000))")
-conn.close
+db_create_conn = PG.connect(dbname: 'postgres')
+db_existence = db_create_conn.exec("SELECT datname FROM pg_catalog.pg_database WHERE lower(datname) = lower('sinatra_memo_app')")
+
+if db_existence.cmd_tuples == 1
+  puts "sinatra_memo_app already exists"
+  table_create_conn = PG.connect(dbname: 'sinatra_memo_app')
+  table_existence = table_create_conn.exec("SELECT * FROM information_schema.tables WHERE table_name = 'memos';")
+  if table_existence.cmd_tuples == 0
+    table_create_conn.exec("CREATE TABLE memos (id serial PRIMARY KEY, title varchar(100), content varchar(10000))")
+  else
+    puts "memos already exists"
+  end
+else
+  db_create_conn.exec("CREATE DATABASE sinatra_memo_app")
+  table_create_conn = PG.connect(dbname: 'sinatra_memo_app')
+  table_create_conn.exec("CREATE TABLE memos (id serial PRIMARY KEY, title varchar(100), content varchar(10000))")
+end
+
+db_create_conn.close
+table_create_conn.close
