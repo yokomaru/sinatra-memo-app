8eabf11ab418630de8fcef41e8a0d9d8838e5d6f
yokomaru <cabochetoganlan@gmail.com>
2024-06-18 01:18:19 +0900

    fix: setup
    前の分岐と合わせてテーブル作成の分岐を逆にする
    メソッドで囲む

--- db/setup.rb
+++ db/setup.rb
@@ -1,22 +1,26 @@
 require 'pg'
 
-db_create_conn = PG.connect(dbname: 'postgres')
-db_existence = db_create_conn.exec("SELECT datname FROM pg_catalog.pg_database WHERE lower(datname) = lower('sinatra_memo_app')")
+def setup
+  db_create_conn = PG.connect(dbname: 'postgres')
+  db_existence = db_create_conn.exec("SELECT datname FROM pg_catalog.pg_database WHERE lower(datname) = lower('sinatra_memo_app')")
 
-if db_existence.cmd_tuples == 1
-  puts "sinatra_memo_app already exists"
-  table_create_conn = PG.connect(dbname: 'sinatra_memo_app')
-  table_existence = table_create_conn.exec("SELECT * FROM information_schema.tables WHERE table_name = 'memos';")
-  if table_existence.cmd_tuples == 0
-    table_create_conn.exec("CREATE TABLE memos (id serial PRIMARY KEY, title varchar(100), content varchar(10000))")
+  if db_existence.cmd_tuples == 1
+    puts "sinatra_memo_app already exists"
+    table_create_conn = PG.connect(dbname: 'sinatra_memo_app')
+    table_existence = table_create_conn.exec("SELECT * FROM information_schema.tables WHERE table_name = 'memos';")
+    if table_existence.cmd_tuples == 1
+      puts "memos already exists"
+    else
+      table_create_conn.exec("CREATE TABLE memos (id serial PRIMARY KEY, title varchar(100), content varchar(10000))")
+    end
   else
-    puts "memos already exists"
+    db_create_conn.exec("CREATE DATABASE sinatra_memo_app")
+    table_create_conn = PG.connect(dbname: 'sinatra_memo_app')
+    table_create_conn.exec("CREATE TABLE memos (id serial PRIMARY KEY, title varchar(100), content varchar(10000))")
   end
-else
-  db_create_conn.exec("CREATE DATABASE sinatra_memo_app")
-  table_create_conn = PG.connect(dbname: 'sinatra_memo_app')
-  table_create_conn.exec("CREATE TABLE memos (id serial PRIMARY KEY, title varchar(100), content varchar(10000))")
+
+  db_create_conn.close
+  table_create_conn.close
 end
 
-db_create_conn.close
-table_create_conn.close
+setup
